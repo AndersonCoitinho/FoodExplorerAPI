6e811e263dcd10e16b14e5859c327db8c2c53085
Anderson Coitinho <104800828+AndersonCoitinho@users.noreply.github.com>
2023-07-25 15:06:39 -0300

    add routes on server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require("express"); //importanto express 
 
+const routes = require("./routes")
+
 const app = express(); //inicializando express
 app.use(express.json());//API vai "entender" que vai receber as respostas em json
+
+app.use(routes)
 
 const PORT = 3333;//definindo a porta de execução
 
