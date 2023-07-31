a5de1dbc629f4a76e653a1a1ab2334fea138a6f4
Anderson Coitinho <104800828+AndersonCoitinho@users.noreply.github.com>
2023-07-31 16:29:48 -0300

    show e delete foodplates

--- src/controllers/FoodPlatesController.js
+++ src/controllers/FoodPlatesController.js
@@ -11,6 +11,24 @@ class FoodPlatesController {
 
   }
 
+  async show(request, response) {
+    const { plates_id } = request.params;
+    
+        const plates = await knex("foodplates").where({plates_id}).first()
+
+        return response.json(plates)
+
+  }
+
+  async delete (request, response) {
+    const { plates_id } = request.params;
+
+    await knex("foodplates").where({plates_id}).delete();
+
+    return response.json();
+
+  }
+
 }
 
 module.exports = FoodPlatesController;
--- src/routes/foodPlates.routes.js
+++ src/routes/foodPlates.routes.js
@@ -6,5 +6,7 @@ const foodPlatesRoutes = Router();
 const foodPlatesController = new FoodPlatesController();
 
 foodPlatesRoutes.post("/", foodPlatesController.create)
+foodPlatesRoutes.get("/:plates_id", foodPlatesController.show)
+foodPlatesRoutes.delete("/:plates_id", foodPlatesController.delete)
 
 module.exports = foodPlatesRoutes;
