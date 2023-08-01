aa3d81f2657681cadabb3b80e8e94fdf08da28f2
Anderson Coitinho <andercoitinho@gmail.com>
2023-08-01 20:41:42 -0300

    validating user and password

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express")
 
 const userRouter = require("./users.routes")
 const foodPlatesRouter = require("./foodPlates.routes")
+const sessionsRouter = require("./sessions.routes")
 
 const routes = Router();
 
 routes.use("/users", userRouter)
 routes.use("/plates", foodPlatesRouter)
+routes.use("/sessions", sessionsRouter)
 
 module.exports = routes;
