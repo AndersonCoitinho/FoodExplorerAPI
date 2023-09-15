9dca856bfd9d7c47671c2f7d0212bbd140573c66
Anderson Coitinho <104800828+AndersonCoitinho@users.noreply.github.com>
2023-09-15 13:54:10 -0300

    save photo bd

--- src/routes/foodPlates.routes.js
+++ src/routes/foodPlates.routes.js
@@ -1,9 +1,13 @@
 const { Router } = require("express")
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const FoodPlatesController = require("../controllers/FoodPlatesController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const foodPlatesRoutes = Router();
+const upload = multer(uploadConfig.MULTER)
+
 const foodPlatesController = new FoodPlatesController();
 
 foodPlatesRoutes.use(ensureAuthenticated)
@@ -12,5 +16,9 @@ foodPlatesRoutes.get("/", foodPlatesController.index)
 foodPlatesRoutes.post("/", foodPlatesController.create)
 foodPlatesRoutes.get("/:plates_id", foodPlatesController.show)
 foodPlatesRoutes.delete("/:plates_id", foodPlatesController.delete)
+foodPlatesRoutes.patch("/photo", ensureAuthenticated, upload.single("photo"), (req, res) => {
+  console.log(req.file.filename);
+  res.json()
+})
 
 module.exports = foodPlatesRoutes;
