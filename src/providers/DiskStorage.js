c0413017c17459f919a99e45740fd0fe095b1842
Anderson Coitinho <104800828+AndersonCoitinho@users.noreply.github.com>
2023-09-15 13:13:24 -0300

    config upload image

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const uploadConfig = require('../configs/uploadConfig');
+
+class DiskStorage {
+  async saveFile(file) {
+    await fs.promises.rename( //MUDA ARQUIVO DE PASTA
+        path.resolve(uploadConfig.TMP_FOLDER, file),
+        path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+    );
+
+    return file;
+  }
+
+  
+}
+
+module.exports = DiskStorage
