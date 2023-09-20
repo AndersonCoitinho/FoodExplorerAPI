bac2f820d3f364c12bacc516ab16f110c23462d8
Anderson Coitinho <104800828+AndersonCoitinho@users.noreply.github.com>
2023-09-20 14:30:41 -0300

    save photo dish bd

--- src/controllers/FoodPlatesAvatarController.js
+++ src/controllers/FoodPlatesAvatarController.js
@@ -4,28 +4,21 @@ const DiskStorage = require("../providers/DiskStorage");
 
 class FoodPlatesAvatarController {
   async update(request, response) {
-    const plates_id = request.params
-    const avatarFileName = request.file.filename;
-
+    const { plates_id } = request.params;
+    const photoFilename  = request.file.filename;
     const diskStorage = new DiskStorage();
+    const foodplates = await knex("foodplates").where({ plates_id: plates_id }).first()
 
-    const foodplates = await knex("foodplates").where({ plates_id }).first();
+    if (!foodplates) {
+      throw new AppError("Prato de comida não encontrado", 404);
+    }
 
-    /*if (!foodplates_id) {
-      throw new AppError("Somente usuários autenticados podem mudar o avatar", 401);
-    }*/
-
-    /*if(foodplates.photo) {
-      await diskStorage.deleteFile(foodplates.photo)
-    }*/
-    
-    const filename = await diskStorage.saveFile(avatarFileName);
+    const filename = await diskStorage.saveFile(photoFilename);
     foodplates.photo = filename
 
-    await knex("foodplates").update(foodplates).where({ id: plates_id});
-    
-    return response.json({foodplates});
+    await knex("foodplates").update(foodplates).where({ plates_id: plates_id });
 
+    return response.json(foodplates);
   }
 }
 
